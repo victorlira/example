c2fc564c17ef402c43414a10ed9234c16ae69694
antonio <antonio.rec@gmail.com>
2025-02-21 18:53:49 -0300

    third changes

--- Example/src/Calculadora.java
+++ Example/src/Calculadora.java
@@ -7,8 +7,8 @@ public class Calculadora {
 	}
 
 	int somar(int n1, int n2) {
-		int resultado = n1+n2;
-		return resultado;
+		int res = n1+n2;
+		return res;
 	}
 	
 	int multiply(int n1, int n2) {
