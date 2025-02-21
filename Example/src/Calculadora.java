762fbbf06a018ce8ef41db049dffec1d91385996
antonio <antonio.rec@gmail.com>
2025-02-21 18:22:07 -0300

    left changes

--- Example/src/Calculadora.java
+++ Example/src/Calculadora.java
@@ -2,8 +2,8 @@
 public class Calculadora {
 	
 	int subtrair(int n1, int n2) {
-		int resultado = n1 - n2;
-		return resultado;
+		int res = n1 - n2;
+		return res;
 	}
 
 	int somar(int n1, int n2) {
